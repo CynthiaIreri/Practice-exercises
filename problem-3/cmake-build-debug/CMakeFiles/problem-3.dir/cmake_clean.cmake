4fa7fa1a2bf1815f120ca582d1a8246ae9e560f7
Jared Wines <jwines760@gmail.com>
2018-01-12 20:29:24 -0800

    fd

---
+++ problem-3/cmake-build-debug/CMakeFiles/problem-3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problem-3.dir/main.cpp.obj"
+  "problem-3.pdb"
+  "problem-3.exe"
+  "problem-3.exe.manifest"
+  "libproblem-3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/problem-3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
